2681fd56a71ddf23250eea4c95471320268637e6
Bump.yml <ghactions@portsbuild.org>
2026-08-20 15:57:02 +0000

    Bump builder versions [testos=freebsd]

--- anyvm.py
+++ anyvm.py
@@ -142,7 +142,7 @@ OPENBSD_E1000_RELEASES = {"7.3", "7.4", "7.5", "7.6"}
 
 
 DEFAULT_BUILDER_VERSIONS = {
-    "freebsd": "2.2.5",
+    "freebsd": "2.2.6",
     "openbsd": "2.0.9",
     "netbsd": "2.2.2",
     "dragonflybsd": "2.0.6",
